74eb489fa082acb72dc7450f49ef0a32b19f7f24
Regina Lu <rlu7@binghamton.edu>
2023-01-30 17:27:52 -0500

    finish

--- ch01/exercises/data.py
+++ ch01/exercises/data.py
@@ -7,4 +7,5 @@ print(15 % 10) #5
 print(10 % 15) #10
 print(10 % 10) #0
 print(0 % 10) #0
-print(10 / 15) #0.6666666666
+print(10 / 15) #2/3
+# the correct answer is 2/3 but since the computer can only hold onto a certain number of bytes, it cuts off the float
