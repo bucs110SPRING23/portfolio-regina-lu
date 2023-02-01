b5c7c0a9757a320ecb880b54c11909b9e80f9b4c
Regina Lu <rlu7@binghamton.edu>
2023-02-01 17:52:45 -0500

    currency

---
+++ ch01/exercises/currency.py
@@ -0,0 +1,5 @@
+rate = int(input("What is the current exchange rate for the Euro to Dollar? "))
+amount = int(input("How much Euro currency would you like to exchange to USD? "))
+total = amount*rate
+result = total - 3
+print(result)
