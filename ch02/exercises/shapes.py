06161b3d8dfd14fd85e0fa2dc7050618d8d953b4
Regina Lu <rlu7@binghamton.edu>
2023-02-15 17:11:35 -0500

    solution

--- ch02/exercises/shapes.py
+++ ch02/exercises/shapes.py
@@ -3,14 +3,23 @@ pygame.init()
 
 screen = pygame.display.set_mode()
 
-pygame.draw.circle(screen, 'red', [200, 150], 50)
-pygame.display.flip()
-pygame.time.wait(1000)
+dimensions = screen.get_size()
+starting_point = [dimensions[0] // 2, dimensions [1] // 2 + 175]
 
-pygame.draw.circle(screen, 'red', [200, 250], 75)
-pygame.display.flip()
-pygame.time.wait(1000)
+# draw library
 
-pygame.draw.circle(screen, 'red', [200, 375], 100)
+# where to draw it
+# color: "red", [r, g, b] => [0-255, 0-255, 0-255]
+# starting point: [x, y]
+# radius: 50
+
+radius = 200
+for _ in range(3):
+    pygame.draw.circle(screen, "red", starting_point, radius)
+    # move center of second circle upwards
+    starting_point[1] = starting_point[1] - radius
+    radius = radius // 2
+    starting_point[1] = starting_point[1] - radius
+
 pygame.display.flip()
-pygame.time.wait(1000)
+pygame.time.wait(2000)
