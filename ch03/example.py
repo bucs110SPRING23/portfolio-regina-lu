1fa153d87c2f720a2a6ca339f98cce19fabf3a69
Regina Lu <rlu7@binghamton.edu>
2023-02-20 17:34:46 -0500

    guess

--- ch03/example.py
+++ ch03/example.py
@@ -112,3 +112,50 @@ for i in range(num + 1):
         print("fizz")
     elif i % 5 == 0:
         print("buzz")
+
+# 2/20
+import pygame
+import random
+
+pygame.init()
+screen = pygame.display.set_mode()
+colors = ["red", "green", "blue", "yellow"]
+random.shuffle(colors)
+
+for color in colors:
+    screen.fill(color)
+    pygame.display.flip()
+    pygame.time.wait(250)
+
+message = """
+    Simon says:
+    UP: RED
+    DOWN: BLUE
+    LEFT: GREEN
+    RIGHT: YELLOW
+
+    click on the window, enter sequence,m press enter
+"""
+
+response = input(message)
+
+#pygame.EVENT_OBJECT
+
+user_sequence = []
+for event in pygame.event.get():
+    if event.type == pygame.KEYDOWN:
+        if event == pygame.K_UP:
+            screen.fill("red")
+            user_sequence.append("red")
+        elif event.key == pygame.K_DOWN:
+            screen.fill("blue")
+            user_sequence.append("blue")
+        elif event.key == pygame.K_LEFT:
+            screen.fill("green")
+            user_sequence.append("green")
+        elif event.key == pygame.K_RIGHT:
+            screen.fill("yellow")
+            user_sequence.append("yellow")
+
+print("user sequence: ", user_sequence)
+print("colors: ", colors)
