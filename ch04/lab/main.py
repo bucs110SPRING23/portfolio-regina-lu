c907457114e85681347c659f29802777aba03dc9
Regina Lu <rlu7@binghamton.edu>
2023-04-19 15:03:03 -0400

    fixed lab

--- ch04/lab/main.py
+++ ch04/lab/main.py
@@ -20,84 +20,76 @@ pygame.draw.line(screen, "black", (screen_size_variable[1] / 2, 0), (screen_size
 pygame.display.flip()
 pygame.time.wait(2000)
 
-pygame.draw.polygon(screen, "green", [(50, 50), (100, 50), (100, 100), (50, 100)])
-pygame.display.flip()
-pygame.time.wait(2000)
+player1_box = pygame.draw.rect(screen, "green", (50,50,100,100))
+player2_box = pygame.draw.rect(screen, "blue", (250,50,100,100))
 
-pygame.draw.polygon(screen, "blue", [(650, 50), (700, 50), (700, 100), (650, 100)])
+font = pygame.font.Font(None, 30)
+text = font.render("Select the winning player:", True, "white")
+screen.blit(text, (50, 25))
 pygame.display.flip()
-pygame.time.wait(2000)
 
 player1 = 0
 player2 = 0
 
-for event in pygame.event.get():
-    if event.type == pygame.MOUSEBUTTONDOWN:
-        if event.button == 1:
-            (mousex, mousey) = pygame.mouse.get_pos()
-            for tries in range(0,11):
-                x = random.randint(0, screen_size_variable[1] + 1)
-                y = random.randint(0, screen_size_variable[1] + 1)
-                distance_from_center = math.hypot(x - screen_size_variable[1] / 2, y - screen_size_variable[1] / 2) #the distance formula
-                is_in_circle = distance_from_center <= screen_size_variable[1]/2 #screen width
-                if is_in_circle == True:
-                    pygame.draw.circle(screen, "green", (x,y), 5)
-                    player1 = player1 + 1
-                    pygame.display.flip()
-                    pygame.time.wait(1000)
-                else:
-                    pygame.draw.circle(screen, "red", (x,y), 5)
-                    pygame.display.flip()
-                    pygame.time.wait(1000)
-                x = random.randint(0, screen_size_variable[1] + 1)
-                y = random.randint(0, screen_size_variable[1] + 1)
-                distance_from_center = math.hypot(x - screen_size_variable[1] / 2, y - screen_size_variable[1] / 2) #the distance formula
-                is_in_circle = distance_from_center <= screen_size_variable[1]/2 #screen width
-                if is_in_circle == True:
-                    pygame.draw.circle(screen, "blue", (x,y), 5)
-                    player2 = player2 + 1
-                    pygame.display.flip()
-                    pygame.time.wait(1000)
-                else:
-                    pygame.draw.circle(screen, "white", (x,y), 5)
-                    pygame.display.flip()
-                    pygame.time.wait(1000)
+selected_player = None
+game_result = None
+running = True
 
-if 50 <= mousex <= 100 and 50 <= mousey <= 100:
-    if player1 > player2:
-        font = pygame.font.Font(None, 48)
-        text = font.render("PLAYER 1 WINS! You guessed correct!", True, "white")
-        screen.blit(text, (50, screen_size_variable[1] / 2)) # where <x> and<y> are coordinates on screen
-        pygame.display.flip()
-        pygame.time.wait(1000)
-    elif player1 < player2:
-        font = pygame.font.Font(None, 48)
-        text = font.render("PLAYER 2 WINS! You guessed incorrectly", True, "white")
-        screen.blit(text, (50, screen_size_variable[1] / 2))
-        pygame.display.flip()
-        pygame.time.wait(1000)
-    else:
-        font = pygame.font.Font(None, 48)
-        text = font.render("TIE! You guessed incorrectly", True, "white")
-        screen.blit(text, (50, screen_size_variable[1] / 2))
-        pygame.display.flip()
-        pygame.time.wait(1000)
-elif 650 <= mousex <= 700 and 50 <= mousey <= 100:
-    if player1 > player2:
-        font = pygame.font.Font(None, 48)
-        text = font.render("PLAYER 1 WINS! You guessed incorrectly!", True, "white")
-        screen.blit(text, (50, screen_size_variable[1] / 2)) # where <x> and<y> are coordinates on screen
-        pygame.display.flip()
-        pygame.time.wait(1000)
-    elif player1 < player2:
-        font = pygame.font.Font(None, 48)
-        text = font.render("PLAYER 2 WINS! You guessed correctly!", True, "white")
-        screen.blit(text, (50, screen_size_variable[1] / 2))
-        pygame.display.flip()
-        pygame.time.wait(1000)
-    else:
-        font = pygame.font.Font(None, 48)
-        text = font.render("TIE! You guessed incorrectly!", True, "white")
-        screen.blit(text, (50, screen_size_variable[1] / 2))
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_position = pygame.mouse.get_pos()
+            if (player1_box.collidepoint(mouse_position)) or (player2_box.collidepoint(mouse_position)):
+                if (player1_box.collidepoint(mouse_position)):
+                    selected_player = "player1"
+                elif (player2_box.collidepoint(mouse_position)):
+                    selected_player = "player2"
+                for tries in range(0,11):
+                    x = random.randint(0, screen_size_variable[1] + 1)
+                    y = random.randint(0, screen_size_variable[1] + 1)
+                    distance_from_center = math.hypot(x - screen_size_variable[1] / 2, y - screen_size_variable[1] / 2) #the distance formula
+                    is_in_circle = distance_from_center <= screen_size_variable[1]/2 #screen width
+                    if is_in_circle == True:
+                        pygame.draw.circle(screen, "green", (x,y), 5)
+                        player1 = player1 + 1
+                        pygame.display.flip()
+                        pygame.time.wait(1000)
+                    else:
+                        pygame.draw.circle(screen, "red", (x,y), 5)
+                        pygame.display.flip()
+                        pygame.time.wait(1000)
+                    x = random.randint(0, screen_size_variable[1] + 1)
+                    y = random.randint(0, screen_size_variable[1] + 1)
+                    distance_from_center = math.hypot(x - screen_size_variable[1] / 2, y - screen_size_variable[1] / 2) #the distance formula
+                    is_in_circle = distance_from_center <= screen_size_variable[1]/2 #screen width
+                    if is_in_circle == True:
+                        pygame.draw.circle(screen, "blue", (x,y), 5)
+                        player2 = player2 + 1
+                        pygame.display.flip()
+                        pygame.time.wait(1000)
+                    else:
+                        pygame.draw.circle(screen, "white", (x,y), 5)
+                        pygame.display.flip()
+                        pygame.time.wait(1000)
+    if selected_player:
+        if player1 > player2:
+            game_result = "player1"
+            if selected_player == game_result:
+                guess_result = "Green won. You have guessed correctly!"
+            else:
+                guess_result = "Blue won. You have guessed incorrectly."
+        elif player1 < player2:
+            game_result = "player2"
+            if selected_player == game_result:
+                guess_result = "Blue won. You have guessed correctly!"
+            else:
+                guess_result = "Green won. You have guessed incorrectly."
+        else:
+            game_result = "Incorrect, it was a tie!"
+        font = pygame.font.Font(None, 30)
+        result_text = font.render(f"Game result: {guess_result}", True, "black")
+        screen.blit(result_text, (50, screen_size_variable[1] / 2))
         pygame.display.flip()
         pygame.time.wait(1000)
