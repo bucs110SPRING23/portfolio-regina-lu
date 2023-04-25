389f72e28bd105f13d5c8af08238873204cffa4c
Regina Lu <rlu7@binghamton.edu>
2023-04-25 02:42:38 -0400

    fixed

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -1,5 +1,4 @@
 import pygame
-
 pygame.init()
 
 # PART A
@@ -15,41 +14,43 @@ def threenp1(n):
 
 def threenp1range(upper_limit):
     threenp1range_iters_dict = {}
-    for n in range(2,upper_limit + 1):
+    for n in range(2, upper_limit + 1):
         iters = threenp1(n)
         threenp1range_iters_dict[n] = iters
     return threenp1range_iters_dict
 
 # PART B
 def graph_coordinates(threenplus1_iters_dict):
-    coordinates = threenplus1_iters_dict.items()
-    for i in coordinates:
-        print(i)  
+    coordinates = []
+    for key,value in threenplus1_iters_dict.items():
+        coordinates.append((key, value)) 
 
-display = pygame.display.set_mode()
-new_display = pygame.transform.flip(display, False, True)
-width, height = new_display.get_size()
-new_display = pygame.transform.scale(new_display, (width * 5, height * 5))
+    display = pygame.display.set_mode()
+    display.fill("red")
+    pygame.draw.lines(display, "white", False, coordinates, width = 1)
+    new_display = pygame.transform.flip(display, False, True)
+    width, height = new_display.get_size()
+    new_display = pygame.transform.scale(new_display, (width, height))
+    display.blit(new_display, (0, 0))
+    pygame.display.flip
 
-display.blit(new_display, (0, 0))
 
-def maxnum(threenplus1_iters_dict):
-    max_so_far = 0
-    for item in threenplus1_iters_dict.values():
-        if item > max_so_far:
-            max_so_far = max_so_far
-    return max_so_far
+    max_so_far = [0,0]
+    for key, value in threenplus1_iters_dict.items():
+        if value >= max_so_far[1]:
+            max_so_far [1] = value
+            max_so_far [0] = key
 
-max_so_far = str(maxnum(threenp1range(10)))
-
-font = pygame.font.Font(None, 50)
-msg = font.render(max_so_far, True, "white")
-display.blit(msg, (10,10))
+    font = pygame.font.Font(None, 50)
+    msg = font.render("The largest number of iterations for the value " + str(max_so_far[0]) + " is " + str(max_so_far[1]), True, "white")
+    display.blit(msg, (10,10))
+    pygame.display.flip()
+    pygame.time.wait(2000)
 
 def main():
-    print(threenp1(21))
-    print(threenp1range(25))
-    graph_coordinates(threenp1range(10))
-    maxnum(threenp1range(10))
+    #pygame.init()
+    upperlimit = int(input("Enter an upper limit: "))
+    print(threenp1range(upperlimit))
+    graph_coordinates(threenp1range(upperlimit))
 
 main()
--- ch08/exercises/project/src/controller.py
+++ ch08/exercises/project/src/controller.py
@@ -1,20 +1,72 @@
 import pygame 
 
+import pygame_menu
+
+from snowman import Snowman
+
 class Controller:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display
-        self.background_color = (200, 200, 250)
-        self.background.fill(self.background_color)
-        self.snowpeoples
-        self.max_snowpeoples = 20
-        number_of_snowpeoples = 2
-        interval = self.screen.get_width() // (number_of_snowpeoples + 1)
-        x = interval
-        for _ in range(number_of_snowpeoples):
-            
+
+        self.screen = pygame.display.set_mode()
+        self.width, self.height = pygame.display.get_window_size()
+
+        ## Bouncing Ball Model
+        self.ball = Ball(self.width / 2, self.height / 2, 100)
+        self.sprites = pygame.sprite.Group((self.ball,))
 
     def mainloop(self):
-        while True:
+
+        while self.state == "GAME":
+
             for event in pygame.event.get():
-                if event.type ==
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if self.ball.rect.collidepoint(event.pos):
+                        pygame.quit()
+                        exit()
+
+            self.sprites.update()
+
+            # adjust the direction of the ball based on position
+            if self.ball.rect.x < 0:
+                self.ball.direction = "right"
+            elif self.ball.rect.x > (self.width - self.ball.rect.width):
+                self.ball.direction = "left"
+
+            self.screen.fill("purple")
+
+            self.sprites.draw(self.screen)
+
+            pygame.display.flip()
+    
+    def __init__(self):
+        pygame.init()
+        ...
+        self.state = "GAME"
+    
+    def gameloop(self):
+        while self.state == "GAME":
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if self.ball.rect.collidepoint(event.pos):
+                        self.state = "END"
+    def mainloop(self):
+        while True:
+            if self.state == "GAME":
+                self.gameloop()
+            elif self.state =="END":
+                self.endloop()
+    def endloop(self):
+        font_obj = pygame.font.SysFont(None, 48)
+        msg = font_obj.render("You win! You may quit any time by closing the program", True, "yellow")
+
+        while self.state == "END":
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+
+
+            self.screen.blit(msg, (10, 10))
+            pygame.display.flip()
